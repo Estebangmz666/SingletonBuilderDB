dac43e86950d7aa982818b0552033b6699cda2a7
Esteban Gómez <142446327+Estebangmz666@users.noreply.github.com>
2024-02-23 08:32:09 -0500

    Created User.java
    User can connect to the database from its own class.

---
+++ SingletoneBuilderDB/src/main/java/org/example/User.java
@@ -0,0 +1,13 @@
+public class user{
+  private String id;
+  private String name;
+
+  public user(String id, String name){
+    this.user = user;
+    this.name = name;
+  }
+
+  public void userDatabaseConnection(){
+    DatabaseConnector.getinstance();
+    Connection.databaseConnection();
+}
